6ec03e3b3b4cb674e548bf4fb0364dee88abe41d
Alex <sasha.mishakov@gmail.com>
2021-06-16 23:37:26 +0300

    AlexMishakov
    add start game function and generation game grid

--- classes.py
+++ classes.py
@@ -1,6 +1,5 @@
 import os
 import curses
-import sys
 
 class Window:
     wind = None
@@ -8,14 +7,26 @@ class Window:
     body = ""
     footer = ""
 
+    colorUser1 = 2
+    colorUser2 = 208
+    selectColor = 241
+    bgColor = curses.COLOR_BLACK #241
+    textColor = 255
+
+    backButton = ("\x1b", "q", "Q")
+    topButton = ("KEY_UP")
+
     def __init__(self):
-        print("Start wm")
+        print("Start window")
 
-    def menu(self, win, menuList, selectMenu):        
-        self.wind = win
+    # START menu
+    def showMenu(self, menuList):
+        selectMenu = [0]
+        curses.wrapper(self.menu, menuList, selectMenu)
+        return selectMenu[0]
 
-        win.nodelay(True)
-        curses.curs_set(0)
+    def menu(self, win, menuList, selectMenu):
+        self.setSetting(win)
         key = ""
 
         self.body = self.setMenu(menuList, selectMenu[0])
@@ -39,6 +50,10 @@ class Window:
                 if key == "\n":
                     break
 
+                if key in self.backButton:
+                    selectMenu[0] = len(menuList)-1
+                    break
+
                 self.body = self.setMenu(menuList, selectMenu[0])
                 self.updateWin()
             except Exception as e:
@@ -52,38 +67,212 @@ class Window:
                 select = "[*]"
             string += select+" "+array[i]+"\n"
         return string
+    # END menu
 
+    # START other function
     def updateWin(self):
         self.wind.clear()
-        self.wind.addstr(self.header+self.body+self.footer)
+        self.wind.addstr(self.header+"\n", curses.color_pair(1) | curses.A_BOLD)
+        self.wind.addstr(self.body+"\n"+self.footer, curses.color_pair(1))
+        
+    
+    def setSetting(self, win):
+        self.wind = win
+        self.wind.nodelay(True)
+        curses.curs_set(0)
+        curses.init_pair(1, self.textColor, self.bgColor)
+        self.wind.bkgd(' ', curses.color_pair(1))
+    
+    def clear(self):
+        self.header = ""
+        self.body = ""
+        self.footer = ""
+        # self.updateWin()
+    # END other function
 
-    def showMenu(self, menuList):
-        selectMenu = [0]
-        curses.wrapper(self.menu, menuList, selectMenu)
+    # START body text
+    def showBodyText(self, sring):
+        key = [""]
+        curses.wrapper(self.text, sring, key)
+        return key[0]
+
+    def text(self, win, sring, key):
+        self.setSetting(win)
+
+        self.body = sring
+        self.updateWin()
+
+        while True:
+            try:
+                key[0] = win.getkey()
+                break
+            except Exception as e:
+                pass
+    # END body text
+
+    # START game field
+    def showGameField(self, arrayField, user, setRow, setLine):
+        xy = ["", ""]
+        curses.wrapper(self.gameField, arrayField, xy, user, setRow, setLine)
+        return xy[0], xy[1]
+
+    def gameField(self, win, arrayField, xy, user, setRow, setLine):
+        self.setSetting(win)
         
-        return selectMenu[0]
+        curses.init_pair(5, self.textColor, self.selectColor)
+        curses.init_pair(6, self.colorUser1, self.bgColor)
+        curses.init_pair(7, self.colorUser1, self.selectColor)
+        curses.init_pair(8, self.colorUser2, self.bgColor)
+        curses.init_pair(9, self.colorUser2, self.selectColor)
+
+        self.createGrid(win, arrayField, setLine, setRow, user) 
+
+        while True:
+            try:
+                key = win.getkey()
+
+                if key == "KEY_DOWN" and setLine < len(arrayField)-1:
+                    setLine += 1
+
+                if key == "KEY_UP" and setLine > 0:
+                    setLine -= 1
+
+                if key == "KEY_RIGHT" and setRow < len(arrayField)-1:
+                    setRow += 1
+
+                if key == "KEY_LEFT" and setRow > 0:
+                    setRow -= 1
+
+                if key == "\n":
+                    xy[0] = setLine
+                    xy[1] = setRow
+                    arrayField[setLine][setRow] = user
+                    break
+
+                if key in self.backButton:
+                    xy[0] = -1
+                    xy[1] = -1
+                    break
+
+                self.createGrid(win, arrayField, setLine, setRow, user) 
+            except Exception as e:
+                pass
         
+    def createGrid(self, win, arrayField, setLine, setRow, user):
+        win.clear()
+        win.addstr(self.header+"\n", curses.color_pair(1) | curses.A_BOLD)
+        win.addstr("Ходит игрок ", curses.color_pair(1))
+        if user == "X":
+            win.addstr(user, curses.color_pair(6) | curses.A_BOLD)
+        elif user == "O":
+            win.addstr(user, curses.color_pair(8) | curses.A_BOLD)
+        win.addstr("\n\n")
+
+        string = ""
+        lineInt = 0
+        for line in arrayField:
+            rowInt = 0 
+            for row in line:
+                if rowInt == setRow and lineInt == setLine:
+                    if row == "X":
+                        win.addstr(" "+row+" ", curses.color_pair(7) | curses.A_BOLD)
+                    elif row == "O":
+                        win.addstr(" "+row+" ", curses.color_pair(9) | curses.A_BOLD)
+                    else:
+                        win.addstr(" "+row+" ", curses.color_pair(5))
+                else:
+                    if row == "X":
+                        win.addstr(" "+row+" ", curses.color_pair(6) | curses.A_BOLD)
+                    elif row == "O":
+                        win.addstr(" "+row+" ", curses.color_pair(8) | curses.A_BOLD)
+                    else:
+                        win.addstr(" "+row+" ", curses.color_pair(1))
+                
+                if rowInt < len(line)-1:
+                    win.addstr("│", curses.color_pair(1))
+                rowInt += 1
+
+            if lineInt < len(arrayField)-1:
+                endString = ""
+                for i in range(len(line)):
+                    if i < len(line)-1:
+                        endString += "───┼"
+                    else:
+                        endString += "───"
+
+                win.addstr("\n"+endString+"\n", curses.color_pair(1))
+            lineInt += 1
+        win.addstr("\n\n"+self.footer, curses.color_pair(1))
+    # END game field
 
 class Game:
-    def __init__(self):
-        print("start gm")
-        self.win = Window()
-        self.menu()
+    lenGrid = 3
 
-    def menu(self):
-        self.win.header = "Игра крестики-нолики\nМеню:\n\n"
+    def __init__(self):
+        print("Start game")
+        self.default()
+        self.win = Window()
+        self.menuPage()
+
+    def default(self):
+        self.arrayField = [[" " for x in range(self.lenGrid)] for y in range(self.lenGrid)]
+        self.user = "X"
+        self.setLine, self.setRow = 1, 1
+        
+
+    def menuPage(self):
+        self.win.clear()
+        self.win.header = "Игра крестики-нолики\nМеню:\n"
+        self.win.footer = "Для выхода нажмите кнопку ESC или q"
         menuList = ["Локальная", "Сетевая игра", "Инструкция", "Выход"]
         selectMenu = self.win.showMenu(menuList)
 
         if selectMenu == 3:
             exit()
         elif selectMenu == 2:
-            self.info()
+            self.infoPage()
+        elif selectMenu == 1:
+            self.interGame()
+        elif selectMenu == 0:
+            self.localGame()
     
-    def info(self):
+    def infoPage(self):
         self.win.header = "Правила по игре\n\n"
         menuList = ["Назад"]
         selectMenu = self.win.showMenu(menuList)
 
         if selectMenu == 0:
-            self.menu()
+            self.menuPage()
+    
+    def localGame(self):
+        self.win.clear()
+        self.win.header = "Локальная игра вдвоем"
+        self.win.footer = "Чтобы вернуться в меню нажмите кнопку ESC или q"
+        self.setLine, self.setRow = self.win.showGameField(self.arrayField, self.user, self.setRow, self.setLine)
+
+        if self.setLine < 0 and self.setRow < 0:
+            self.default()
+            self.menuPage()
+        
+        if self.user == "X":
+            self.user = "O"
+        else:
+            self.user = "X"
+
+        if self.endGame():
+            print()
+        else:
+            self.localGame()
+        
+    def interGame(self):
+        print()
+
+    def endGame(self):
+        endGameBool = False
+        # for line in len(self.arrayField):
+        #     for row in len(line):
+        #         print()
+
+        return endGameBool
+
+gm = Game()
